076343be1845a94569d0b7bfdacff83bde7f3a6f
Alessandro Joshua Zamproni <zmproni@gmail.com>
2023-01-01 13:13:57 +0800

    Split and load sample data

---
+++ sample_split.py
@@ -0,0 +1,29 @@
+import pymongo
+import time
+
+LIMIT = 100_000
+
+def calculate_elapsed_time(start):
+    elapsed = time.perf_counter() - start
+    return elapsed
+
+# Track the time it takes to insert the data
+start = time.perf_counter()
+
+# Connect to MongoDB
+with pymongo.MongoClient() as client:
+    db = client.expedia
+    tickets = db.tickets.find(limit=LIMIT)
+    for count, ticket in enumerate(tickets):
+        result = db.sample.insert_one(ticket)
+
+        #calculate progress
+        progress = (count  + 1) / LIMIT * 100
+
+        # Print current elapsed time & progress
+        curr_elapsed = calculate_elapsed_time(start)
+        print(f'\rCurrent elapsed time: {curr_elapsed:.2f} seconds\tProgress: {progress:.1f}%', end='')
+
+end = time.perf_counter()
+elapsed = calculate_elapsed_time(start)
+print(f'Elapsed time: {elapsed:.2f} seconds')

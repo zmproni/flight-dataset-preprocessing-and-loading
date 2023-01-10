8aac08168ccbb2a15733d54b9a1054a8f1147238
Alessandro Joshua Zamproni <zmproni@gmail.com>
2023-01-10 10:35:48 +0800

    Update 4th query

--- queries/4th.js
+++ queries/4th.js
@@ -1,7 +1,22 @@
 // 4. Update all documents in the collection with legId = c38a6e4b807d15541e5866676febcbec
 // Set the equipmentDescription field to "Airbus A320-200"
-db.tickets.explain('executionStats').update(
-    {legId: 'c38a6e4b807d15541e5866676febcbec'},
-    {$set: {  equipmentDescription: "Airbus A320-200"}},
-  )
-  
+db.tickets.explain('executionStats').updateMany(
+  { "segments.airlineName": "American Airlines" },
+  {
+    $set: {
+      baseFare: {
+        $cond: {
+          if: { $eq: ['$isNonStop', true] },
+          then: { $add: ['$baseFare', 50] },
+          else: { $add: ['$baseFare', 20] },
+        }
+      },
+      totalFare: {
+        $cond: {
+          if: { $eq: ['$isNonStop', true] },
+          then: { $add: ['$totalFare', 50] },
+          else: { $add: ['$totalFare', 20] },
+        }
+      }
+    },
+  })
